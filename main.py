3f9d4ac45ef815b9cd6460406928add9ae078564
AnlaAnla <wow383107980@qq.com>
2022-12-01 16:06:36 +0800

    更新prizm分类

--- main.py
+++ main.py
@@ -187,6 +187,6 @@ if __name__ == '__main__':
 
     # 是否使用OCR，使用后速度会下降很多，相对于yolo
     use_ocr = False
-    app.debug = True
+    app.debug = False
     print(torch.cuda.is_available())
     app.run()
